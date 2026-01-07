b254677529707ec8ff51802fe0197a0a571a09fd
Haim Vales <haimvales@gmail.com>
2026-01-07 17:32:33 +0200

    addNoteClass

--- models/usersM.js
+++ models/usersM.js
@@ -2,10 +2,12 @@ import { nanoid } from "nanoid"
 
 export class Users {
     constructor(username, password) {
+        this.notes = []
         this.id = nanoid(4)
         this.username = username
         this.password = password
         this.createdAt = new Date().toISOString()
+        this.genarateNote()
     }
     returenObj() {
         return {
@@ -13,6 +15,12 @@ export class Users {
             username: this.username,
             password: this.password,
             createdAt: this.createdAt
+        }
+    }
+    genarateNote(arr = 2) {
+        for (let i = 0; i < arr.length; i++) {
+            const element = new Note(this.username, `Example: ${i + 1}`)
+            this.notes.push(element)
         }
     }
 }
