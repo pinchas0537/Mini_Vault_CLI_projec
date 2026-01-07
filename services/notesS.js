e8d15fd102dea17bb2188480e847aba0ae4c1e26
Pinchas Lifshitz <pnl8480537@gmail.com>
2026-01-07 17:47:32 +0200

    finish listNotes func

--- services/notesS.js
+++ services/notesS.js
@@ -1,23 +1,24 @@
-import {add,listByOwner} from "../DAL/notesD.js"
-import {Note} from "../models/notesM.js"
-import {listByOwner} from "../DAL/notesD.js"
+import { add, listByOwner } from "../DAL/notesD.js"
+import { Note } from "../models/notesM.js"
+import { listByOwner } from "../DAL/notesD.js"
 
-export function listNotes(ownerUsername){
-
-    try{
-        if(typeof(ownerUsername)==="string"){
-            const listNotes_by_username=listByOwner(ownerUsername)
+export function listNotes(ownerUsername) {
+    try {
+        if (typeof (ownerUsername) === "string") {
+            const listNotes_by_username = listByOwner(ownerUsername)
             return listNotes_by_username
-    }else{
-        console.log("The ownerUsername not string.")
-    }}catch{error}{
+        } else {
+            console.log("The ownerUsername not string.")
+        }
+    } catch { error } {
         console.error(error)
     }
+}
 
-export function addNote(ownerUsername, text){
-    const new_note=new Note(
+export function addNote(ownerUsername, text) {
+    const new_note = new Note(
         ownerUsername,
         text)
-    const insert=add(new_note)
+    const insert = add(new_note)
     return insert
 }
